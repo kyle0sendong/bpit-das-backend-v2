29df2d754c01a8ccafced540ad4dee06eaea301c
Kyle Destura <kyle.afundar@gmail.com>
2025-01-06 16:06:44 +0800

    Cleaned and make Tcp Analyzer API functional

--- src/api/v1/analyzers/tcp/tcpAnalyzerController.js
+++ src/api/v1/analyzers/tcp/tcpAnalyzerController.js
@@ -1,44 +1,38 @@
 const asyncHandler = require('express-async-handler');
-const tcpAnalyzerModel = require('./tcpAnalyzerModel');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.json());
+const TcpAnalyzerModel = require("./TcpAnalyzerModel");
 
-const asyncErrorHandler = (func) => {
-    return (req, res, next) => {
-        func(req, res, next).catch(error => next(error))
+class TcpAnalyzerController {
+  
+  getTcpAnalyzer = asyncHandler( async(req, res) => {
+    const id = req.query.id ?? 0;
+    if(id > 0) {
+      const result = await TcpAnalyzerModel.getById(req.query.id);
+      res.status(200).json(result);
+    } else {
+      const result = await TcpAnalyzerModel.getAll();
+      res.status(200).json(result);
     }
+  })
+
+  insertTcpAnalyzer = asyncHandler(async(req, res) => {
+    await TcpAnalyzerModel.insert(req.body);
+    res.status(200).send(`Inserted ${req.body.name} Modbus TCP`);
+  })
+    
+  updateTcpAnalyzer = asyncHandler(async(req, res) => {
+    await TcpAnalyzerModel.update(req.body)
+    res.status(200).send(`Update Success`)
+  })
+  
+  deleteTcpAnalyzer = asyncHandler( async(req, res) => {
+    await TcpAnalyzerModel.delete(req.body.id)
+    res.status(200).send(`Deleted ${req.body.name} TCP Analyzer`)
+  })
+
+  getTcpWithParametersById = asyncHandler(async(req, res) => {
+    tcpAnalyzerWithParameters = await TcpAnalyzerModel.getTcpWithParametersById(req.body.id)
+    res.status(200).json(tcpAnalyzerWithParameters)
+  })
 }
 
-const insertTcpAnalyzer = asyncErrorHandler(async(req, res) => {
-    await tcpAnalyzerModel.insertTcpAnalyzer(req.body)
-    res.status(200).send(`Inserted ${req.body.name} Modbus TCP`)
-})
-
-const getTcpAnalyzer = asyncHandler( async(req, res) => {
-    const tcpAnalyzer = await tcpAnalyzerModel.getTcpAnalyzer(req.params.id)
-    res.status(200).json(tcpAnalyzer)
-})
-
-const getAllTcpAnalyzer = asyncErrorHandler(async(req, res) => {
-    const tcpAnalyzers = await tcpAnalyzerModel.getAll()
-    res.status(200).json(tcpAnalyzers)
-})
-
-const getTcpWithParameters = asyncErrorHandler(async(req,res) => {
-    const tcpAnalyzerWithParameters = await tcpAnalyzerModel.getTcpWithParameters(req.body.id)
-    res.status(200).json(tcpAnalyzerWithParameters)
-})
-
-const updateTcpAnalyzer = asyncHandler(async(req, res) => {
-    await tcpAnalyzerModel.updateTcpAnalyzer(req.body)
-    res.status(200).send(`Update Success`)
-})
-
-const deleteTcpAnalyzer = asyncHandler( async(req, res) => {
-    await tcpAnalyzerModel.deleteTcpAnalyzer(req.body.id)
-    res.status(200).send(`Deleted ${req.body.name} TCP Analyzer`)
-})
-
-module.exports = {insertTcpAnalyzer, getTcpAnalyzer, getAllTcpAnalyzer, updateTcpAnalyzer, deleteTcpAnalyzer, getTcpWithParameters}
+module.exports = new TcpAnalyzerController();
--- src/api/v1/analyzers/tcp/tcpAnalyzerModel.js
+++ src/api/v1/analyzers/tcp/tcpAnalyzerModel.js
@@ -1,72 +1,11 @@
-const db = require('@database');
+const parameterModel = require('@apiV1/parameters/ParameterModel');
+const AnalyzerBaseModel = require("../AnalyzerBaseModel");
 
-module.exports = class Model {
-
-    constructor() {
-        this.table = 'tcp_analyzers';
-    }
-
-    getTcpAnalyzer(id) {
-        const query = 'SELECT * FROM ?? WHERE id = ?';
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, id], (error, result) => {
-                if(error) throw error;
-                resolve(result)
-            })
-        })
-    }
-
-    getAll() {
-        const query = 'SELECT * FROM ??'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table], (error, result) => {
-                if(error) throw error;
-                resolve(result)
-            })
-        })
-    }
-
-    async getTcpWithParameters(id) {
-        const query = `SELECT tcp_analyzer.name, parameter.id, parameter.name FROM ??
-        INNER JOIN ?? ON parameter.id = tcp_analyzer.?`
-
-        try {
-            const result = db.query(query, [this.table, 'parameter', id])
-            return result 
-        } catch (error) {
-            throw error
-        }
-    }
-
-    // Schema: name, host_address, port, device_address, sampling
-    insertTcpAnalyzer(data) {
-        const query = 'INSERT INTO ?? (name, host_address, port, device_address, sampling) VALUES (?,?,?,?,?)'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, data.name, data.host_address, data.port, data.device_address, data.sampling], (error, result) => {
-                if(error) throw error;
-                resolve(result)
-            })
-        })
-    }
-
-    updateTcpAnalyzer(data) {
-        const query = 'UPDATE ?? SET ? WHERE id = ?'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, data, data.id], (error, result) => {
-                if(error) throw error;
-                resolve(result);
-            })
-        })
-    }
-
-    deleteTcpAnalyzer(id) {
-        const query = 'DELETE FROM ?? WHERE id = ?'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, id], (error, result) => {
-                if(error) throw error;
-                resolve(result);
-            })
-        })
-    }
-
+class TcpAnalyzerModel extends AnalyzerBaseModel {
+  
+	constructor() {
+    super('tcp_analyzers', parameterModel.getTableName);
+	}
 }
+
+module.exports = new TcpAnalyzerModel();
