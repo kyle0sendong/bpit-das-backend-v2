866df8d953b6a765298fca370e07ff07f6f80647
Kyle Destura <kyle.afundar@gmail.com>
2025-01-06 16:07:13 +0800

    Added Tcp Analyzer on API routes

--- src/api/v1/routes.js
+++ src/api/v1/routes.js
@@ -1,5 +1,11 @@
 const { Router } = require("express");
+const TcpAnalyzerController = require("./analyzers/tcp/TcpAnalyzerController");
 
 const router = Router();
+
+router.get('/tcp-analyzer', TcpAnalyzerController.getTcpAnalyzer);
+router.post('/tcp-analyzer', TcpAnalyzerController.insertTcpAnalyzer);
+router.patch('/tcp-analyzer', TcpAnalyzerController.updateTcpAnalyzer);
+router.delete('/tcp-analyzer', TcpAnalyzerController.deleteTcpAnalyzer);
 
 module.exports = router;
