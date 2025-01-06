83a0b55b1972c92fe49b24e69d1cb31f63b47050
Kyle Destura <kyle.afundar@gmail.com>
2025-01-06 15:05:40 +0800

    Renamed basemodel to ApiBaseModel

---
+++ src/api/ApiBaseModel.js
@@ -0,0 +1,65 @@
+const db = require("@database");
+
+class BaseApiModel {
+  constructor(tableName) {
+    this.tableName = tableName;
+  }
+
+  executeQuery(query, params = []) {
+    return new Promise((resolve, reject) => {
+      db.query(query, params, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+    });
+  }
+
+  getTableName() {
+    return this.tableName;
+  }
+  
+  getAll() {
+    const query = `
+      SELECT *
+      FROM ${this.tableName}
+    `;
+    return this.executeQuery(query);
+  }
+
+  getAllById(id) {
+    const query = `
+      SELECT *
+      FROM ${this.tableName}
+      WHERE id = ?
+    `;
+    return this.executeQuery(query, [id]);
+  }
+
+  insert(data) {
+    const query = `
+      INSERT INTO ${this.tableName}
+      SET ?
+    `;
+    return this.executeQuery(query, [data]);
+  }
+
+  update(data) {
+    const query = `
+      UPDATE ${this.tableName}
+      SET ?
+      WHERE id = ?
+    `;
+    return this.executeQuery(query, [data, data.id]);
+  }
+  
+  delete(id) {
+    const query = `
+      DELETE FROM ${this.tableName}
+      WHERE id = ?
+    `;
+    return this.executeQuery(query, [id]);
+  }
+
+}
+
+module.exports = BaseApiModel;
