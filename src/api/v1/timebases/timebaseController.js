965560da2096540e5200f0206849202d8a00dcd8
Kyle Destura <kyle.afundar@gmail.com>
2025-01-07 11:27:48 +0800

    Cleaned and optimized Timebases API

--- src/api/v1/timebases/timebaseController.js
+++ src/api/v1/timebases/timebaseController.js
@@ -1,29 +1,28 @@
 const asyncHandler = require('express-async-handler')
 const timebaseModel = require('./timebaseModel')
 
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-app.use(bodyParser.json())
+class TimebaseController {
 
-const getAllTimebase = asyncHandler(async(req, res) => {
-    const timebases = await timebaseModel.getAllTimebase()
-    res.status(200).json(timebases)
-})
+  getAllTimebase = asyncHandler(async(req, res) => {
+    const timebases = await timebaseModel.getAll();
+    return res.status(200).json(timebases);
+  })
 
-const insertTimebase = asyncHandler(async(req, res) => {
-    await timebaseModel.insertTimebase(req.body)
-    res.status(200).send(`Inserted ${req.body.timebase} minutes`)
-})
+  insertTimebase = asyncHandler(async(req, res) => {
+    await timebaseModel.insert(req.body);
+    return res.status(200).send(`Inserted ${req.body.timebase} minutes`);
+  })
 
-const updateTimebase = asyncHandler(async(req, res) => {
-    await timebaseModel.updateTimebase(req.body)
-    res.status(201).send('Updated timebase data')
-})
+  updateTimebase = asyncHandler(async(req, res) => {
+    await timebaseModel.update(req.body);
+    return res.status(201).send('Updated timebase data');
+  })
 
-const deleteTimebase = asyncHandler(async(req, res) => {
-    await timebaseModel.deleteTimebase(req.body)
-    res.status(200).send(`Deleted timebase with id:${req.body.id}`)
-})
+  deleteTimebase = asyncHandler(async(req, res) => {
+    await timebaseModel.delete(req.body);
+    return res.status(200).send(`Deleted timebase with id:${req.body.id}`);
+  })
+}
 
-module.exports = {getAllTimebase, insertTimebase, updateTimebase, deleteTimebase}
+
+module.exports = new TimebaseController();
--- src/api/v1/timebases/timebaseModel.js
+++ src/api/v1/timebases/timebaseModel.js
@@ -1,49 +1,9 @@
-const db = require('@database');
+const ApiBaseModel = require("@api/ApiBaseModel");
 
-module.exports = class Model {
-
-    constructor(table) {
-        this.table = 'timebases'
-    }
-
-    // Schema: timebase
-    insertTimebase(data) {
-        const query = 'INSERT INTO ?? (enable, timebase, custom) VALUES (?, ?, ?)'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, data.enable, data.timebase, data.custom], (error, result) => {
-                if(error) throw error;
-                resolve(result)
-            })
-        })
-    }
-
-    getAllTimebase() {
-        const query = 'SELECT * FROM ??'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table], (error, result) => {
-                if(error) throw error;
-                resolve(result);
-            })
-        })
-    }
-
-    updateTimebase(data) {
-        const query = 'UPDATE ?? SET ? WHERE timebase = ?'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, data, data.timebase], (error, result) =>{
-                if(error) throw error;
-                resolve(result);
-            })
-        })
-    }
-
-    deleteTimebase(data) {
-        const query = 'DELETE FROM ?? WHERE timebase = ?'
-        return new Promise( (resolve, reject) => {
-            db.query(query, [this.table, data.timebase], (error, result) => {
-                if(error) throw error;
-                resolve(result);
-            })
-        })
-    }
+class TimebaseModel extends ApiBaseModel {
+  constructor() {
+    super('timebases');
+  }
 }
+
+module.exports = new TimebaseModel();
