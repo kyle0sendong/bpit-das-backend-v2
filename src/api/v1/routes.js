572c36093daa44e629d45f10d7ad0fd9ff54d0cd
Kyle Destura <kyle.afundar@gmail.com>
2025-01-07 11:28:52 +0800

    Added all API controllers on API routes

--- src/api/v1/routes.js
+++ src/api/v1/routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
+const router = Router();
+
 const TcpAnalyzerController = require("./analyzers/tcp/TcpAnalyzerController");
 const ParameterController = require("./parameters/ParameterController");
-
-const router = Router();
+const SiteController = require("./sites/SiteController");
+const TimebaseController = require("./timebases/TimebaseController");
+const UserLogController = require("./user-logs/UserLogController");
+const VirtualChannelController = require("./virtual-channels/VirtualChannelController");
 
 router.get('/tcp-analyzers', TcpAnalyzerController.getTcpAnalyzer);
 router.post('/tcp-analyzers', TcpAnalyzerController.insertTcpAnalyzer);
@@ -13,5 +17,21 @@ router.get('/parameters', ParameterController.getParameters);
 router.post('/parameters', ParameterController.insertParameter);
 router.patch('/parameters', ParameterController.updateParameter);
 router.delete('/parameters', ParameterController.deleteParameter);
+
+router.get('/sites', SiteController.getAllSites);
+router.patch('/sites', SiteController.updateSite);
+
+router.get('/timebases', TimebaseController.getAllTimebase);
+router.post('/timebases', TimebaseController.insertTimebase);
+router.patch('/timebases', TimebaseController.updateTimebase);
+router.delete('/timebases', TimebaseController.deleteTimebase);
+
+router.get('/user-logs', UserLogController.getUserLog);
+router.post('/user-logs', UserLogController.insertLog);
+
+router.get('/virtual-channels', VirtualChannelController.getVirtualChannel);
+router.post('/virtual-channels', VirtualChannelController.insertVirtualChannel);
+router.patch('/virtual-channels', VirtualChannelController.updateVirtualChannel);
+router.delete('/virtual-channels', VirtualChannelController.deleteVirtualChannel);
 
 module.exports = router;
