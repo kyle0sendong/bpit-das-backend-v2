5fecd84b4e8d3a46296a690ea0187d05ddf95584
Kyle Destura <kyle.afundar@gmail.com>
2025-01-07 11:27:07 +0800

    Update tcp analyzer controller

--- src/api/v1/analyzers/tcp/tcpAnalyzerController.js
+++ src/api/v1/analyzers/tcp/tcpAnalyzerController.js
@@ -7,26 +7,26 @@ class TcpAnalyzerController {
     const id = req.query.id ?? 0;
     if(id > 0) {
       const result = await TcpAnalyzerModel.getById(req.query.id);
-      res.status(200).json(result);
+      return res.status(200).json(result);
     } else {
       const result = await TcpAnalyzerModel.getAll();
-      res.status(200).json(result);
+      return res.status(200).json(result);
     }
   })
 
   insertTcpAnalyzer = asyncHandler(async(req, res) => {
     await TcpAnalyzerModel.insert(req.body);
-    res.status(200).send(`Inserted ${req.body.name} Modbus TCP`);
+    return res.status(200).send(`Inserted ${req.body.name} Modbus TCP`);
   })
     
   updateTcpAnalyzer = asyncHandler(async(req, res) => {
     await TcpAnalyzerModel.update(req.body)
-    res.status(200).send(`Update Success`)
+    return res.status(200).send(`Update Success`)
   })
   
   deleteTcpAnalyzer = asyncHandler( async(req, res) => {
     await TcpAnalyzerModel.delete(req.body.id)
-    res.status(200).send(`Deleted ${req.body.name} TCP Analyzer`)
+    return res.status(200).send(`Deleted ${req.body.name} TCP Analyzer`)
   })
 
 }
