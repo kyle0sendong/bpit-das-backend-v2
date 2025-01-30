053ea65b5b55c97072c3c6d1b0c5ab5522d16937
Kyle Destura <kyle.afundar@gmail.com>
2025-01-30 10:29:01 +0800

    fix bug on splitting headers

--- src/api/v1/users/UserController.js
+++ src/api/v1/users/UserController.js
@@ -21,7 +21,8 @@ class UserController {
   });
 
   logoutUser = asyncHandler( async(req, res) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization;
+
     if (!token) return res.status(401).json({ message: 'No token provided.' });
 
     try {
