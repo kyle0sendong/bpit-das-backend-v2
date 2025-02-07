48fa14c9b8bb8e2111dd6356f8a037e13bb613ed
Kyle Destura <kyle.afundar@gmail.com>
2025-02-07 14:58:25 +0800

    Segregate protected and unprotected routes

--- src/api/v1/routes.js
+++ src/api/v1/routes.js
@@ -11,21 +11,23 @@ const CurrentValueController = require("./current-values/CurrentValueController"
 const AnalyzerDataController = require("./analyzer-data/AnalyzerDataController");
 const UserController = require("./users/UserController");
 
-const validateToken = require("@middleware/validateToken")
+const validateToken = require("@middleware/validateToken");
+const checkPrivilege = require("@middleware/checkPrivilege");
 
 // unprotected routes
 router.get('/tcp-analyzers', TcpAnalyzerController.getTcpAnalyzer);
 router.get('/current-values', CurrentValueController.getCurrentValues);
-router.get('/sites', StationController.getAllSites);
-router.get('/timebases', TimebaseController.getAllTimebase);
+
 router.get('/virtual-channels', VirtualChannelController.getVirtualChannel);
 router.get('/tcp-parameters', TcpParameterController.getParameters);
 router.get('/analyzer-data', AnalyzerDataController.getAnalyzerData);
-router.get('/user-logs', UserLogController.getUserLogsByDate);
+
 router.post('/login', UserController.loginUser);
 
 // protected routes
-router.use(validateToken);
+router.get('/sites', StationController.getAllSites);
+
+router.use(validateToken, checkPrivilege(["admin", "integrator"]));
 router.post('/tcp-analyzers', TcpAnalyzerController.insertTcpAnalyzer);
 router.patch('/tcp-analyzers', TcpAnalyzerController.updateTcpAnalyzer);
 router.delete('/tcp-analyzers', TcpAnalyzerController.deleteTcpAnalyzer);
@@ -39,10 +41,12 @@ router.delete('/tcp-parameters', TcpParameterController.deleteParameter);
 
 router.patch('/sites', StationController.updateSite);
 
+router.get('/timebases', TimebaseController.getAllTimebase);
 router.post('/timebases', TimebaseController.insertTimebase);
 router.patch('/timebases', TimebaseController.updateTimebase);
 router.delete('/timebases', TimebaseController.deleteTimebase);
 
+router.get('/user-logs', UserLogController.getUserLogsByDate);
 router.get('/log-distinct-dates', UserLogController.getDistinctDate);
 
 router.post('/virtual-channels', VirtualChannelController.insertVirtualChannel);
@@ -51,12 +55,11 @@ router.delete('/virtual-channels', VirtualChannelController.deleteVirtualChannel
 
 router.post('/logout', UserController.logoutUser);
 
-
 // admin only routes
+router.use(validateToken, checkPrivilege(["admin"]));
 router.get('/users', UserController.getAllUsers);
 router.post('/register', UserController.registerUser);
 router.patch('/user', UserController.updateUser);
 router.delete('/user', UserController.deleteUser);
-
 
 module.exports = router;
