ae54893e9d2227cf46d09158af72a2b01a6e37a8
Kyle Destura <kyle.afundar@gmail.com>
2025-02-10 11:32:34 +0800

    Added logging of user actions for inserting and deleting parameters

--- src/api/v1/parameters/ParameterBaseModel.js
+++ src/api/v1/parameters/ParameterBaseModel.js
@@ -1,50 +1,35 @@
 const AlterTableDataColumnModel = require("@databaseOperations/AlterTableDataColumnModel");
 const ApiBaseModel = require("@api/ApiBaseModel");
 const { toSnakeCase } = require("@utils/strings");
+const TcpAnalyzerModel = require("@apiV1/analyzers/tcp/TcpAnalyzerModel");
+const UserLogModel = require("@apiV1/user-logs/UserLogModel");
+
 class ParameterBaseModel extends ApiBaseModel {
 
   constructor(tableName) {
     super(tableName)
+    this.tcpAnalyzerTable = TcpAnalyzerModel.getTableName();
   }
 
-  async insertParameter(dataArray, type) {
-    const columns = Object.keys(dataArray[0]);
-    const values = dataArray.map(item => columns.map(col => item[col]));
+  getParameterDetailsById(id) {
     const query = `
-      INSERT INTO ${this.tableName} (${columns.join(", ")})
-      VALUES ?
+      SELECT
+        ${this.tableName}.name,
+        ${this.tableName}.unit,
+        ${this.tableName}.enable,
+        ${this.tableName}.request_interval,
+        ${this.tableName}.format,
+        ${this.tableName}.function_code,
+        ${this.tableName}.start_register_address,
+        ${this.tableName}.register_count,
+        ${this.tableName}.formula,
+        ${this.tableName}.analyzer_id,
+        ${this.tcpAnalyzerTable}.name AS analyzer_name
+      FROM ${this.tableName}
+      INNER JOIN ${this.tcpAnalyzerTable} ON ${this.tableName}.analyzer_id = ${this.tcpAnalyzerTable}.id
+      WHERE ${this.tableName}.id = ?
     `;
-    const insertColumnPromises = dataArray.map( (data) =>
-      AlterTableDataColumnModel.insertDataColumn({
-        columnName: toSnakeCase(`${data.name}_${type}${data.analyzer_id}`),
-        dataType: 'decimal(10,5)'
-      })
-    )
-
-    try {
-      return await Promise.all([this.executeQuery(query, [values]), ...insertColumnPromises])
-    } catch(error) {
-      console.error(`Error inserting parameter: `, error);
-    }
-  }
-
-  async deleteParameter(id, type) {
-    const parameter = await this.getById(id);
-
-    const query = `
-      DELETE FROM ${this.tableName}
-      WHERE id = ?
-    `;
-
-    try {
-      await Promise.all([
-        this.executeQuery(query, [id]),
-        AlterTableDataColumnModel.deleteDataColumn({columnName: toSnakeCase(`${parameter[0].name}_${type}${parameter[0].analyzer_id}`)})
-      ])
-    } catch(error) {
-      console.error(`Error inserting parameter: `, error);
-    }
-
+    return this.executeQuery(query, [id]);
   }
 
   getParametersByAnalyzerId(id) {
@@ -55,6 +40,72 @@ class ParameterBaseModel extends ApiBaseModel {
     return this.executeQuery(query, [id]);
   }
 
+  async insertParameter(dataArray, type, user, numberOfParameter, analyzerId = 0) {
+
+    const columns = Object.keys(dataArray[0]);
+    const values = dataArray.map(item => columns.map(col => item[col]));
+    const analyzerDetails = await TcpAnalyzerModel.getById(analyzerId);
+
+    // process log data
+    const logData = {
+      username: user.username,
+      full_name: user.name,
+      tags: "Parameter, Insert",
+      changes: `Inserted ${numberOfParameter} parameter to '${analyzerId > 0 ? analyzerDetails[0].name : 'Virtual Channels'}'`
+    };
+    
+    const query = `
+      INSERT INTO ${this.tableName} (${columns.join(", ")})
+      VALUES ?
+    `;
+
+    // for batch insert, use promise all
+    const insertColumnPromises = dataArray.map( (data) =>
+      AlterTableDataColumnModel.insertDataColumn({
+        columnName: toSnakeCase(`${data.name}_${type}${data.analyzer_id}`),
+        dataType: 'decimal(10,5)'
+      })
+    )
+
+    try {
+      await Promise.all([this.executeQuery(query, [values]), ...insertColumnPromises])
+
+      return UserLogModel.insert(logData);
+
+    } catch(error) {
+      console.error(`Error inserting parameter: `, error);
+    }
+  }
+
+  async deleteParameter(id, type, user) {
+    const parameterDetails = await this.getParameterDetailsById(id);
+    const analyzerDetails = await TcpAnalyzerModel.getById(parameterDetails[0].analyzer_id);
+    const logData = {
+      username: user.username,
+      full_name: user.name,
+      tags: "Parameter, Delete",
+      changes: `Deleted parameter '${parameterDetails[0].name}' from '${analyzerDetails[0].name}'`
+    };
+
+    const query = `
+      DELETE FROM ${this.tableName}
+      WHERE id = ?
+    `;
+
+    try {
+      // delete columns from data tables
+      await Promise.all([
+        this.executeQuery(query, [id]),
+        AlterTableDataColumnModel.deleteDataColumn({columnName: toSnakeCase(`${parameterDetails[0].name}_${type}${parameterDetails[0].analyzer_id}`)})
+      ])
+
+      return UserLogModel.insert(logData);
+    } catch(error) {
+      console.error(`Error inserting parameter: `, error);
+    }
+  }
+
+  
   async updateParameter(dataArray, type) {
 
     const updatePromises = dataArray.map(async (data) => {
@@ -80,6 +131,9 @@ class ParameterBaseModel extends ApiBaseModel {
       console.error("Error updating one or more parameters:", error);
     }
   }
+
+
+
   
   
   
