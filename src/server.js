78e81abb34f7d770cb5fae093d0b8f70b9642809
Kyle Destura <kyle.destura@oqulo.com>
2025-02-18 09:28:33 +0800

    1 test commit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("module-alias/register.js");
 const app = require("@app");
 const dotenv = require("dotenv").config();
 
+console.log("1st commit")
 const port = process.env.PORT || 55001;
 
 app.listen(port, () => {
