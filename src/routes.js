4193274075a4551cc811ba0714bf7aeed2389b4f
Kyle Destura <kyle.afundar@gmail.com>
2025-03-16 19:53:08 +0800

    Add all changes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ const routesV1 = require("@apiV1/routes.js");
 
 const router = Router();
 
-console.log("2nd commit")
 router.use('/v1', routesV1);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ require("module-alias/register.js");
 const app = require("@app");
 const dotenv = require("dotenv").config();
 
-console.log("1st commit")
 const port = process.env.PORT || 55001;
 
 app.listen(port, () => {
